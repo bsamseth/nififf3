3ec0503410e35e2f0544c0d9d6b723a2d5c3ed92
Bendik Samseth <bendik@samseth.me>
2026-03-23 11:08:54 +0800

    Move main.rs into an example
    So that dev dependencies can be removed from main dependencies

--- examples/main.rs
+++ examples/main.rs
@@ -1,13 +1,10 @@
-use futures::{StreamExt, TryStreamExt};
-
 use anyhow::{Context, Result};
 use axum::{
     http::{HeaderMap, HeaderName, HeaderValue},
     response::IntoResponse,
 };
+use futures::{StreamExt, TryStreamExt};
 use tokio::io::AsyncReadExt;
-
-use nifioxide::{FlowFileParsingError, FlowFileStream, axum::StreamedFlowFileFuture};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -19,46 +16,15 @@ async fn main() -> Result<()> {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:9999")
         .await
         .context("binding api server to address")?;
+    tracing::info!("Listening on 127.0.0.1:9999");
     axum::serve(listener, app).await?;
     Ok(())
 }
 
-// Notes
-// Keep the option to _accept_ multiple flow files.
-//
-// However, move to one flow file per request as the intended solution.
-// This because HTTP requires us to decide on a response code before we send a single other byte of
-// the response body. If we don't won't to buffer the whole stream (defeating the streaming
-// approach to begin with), then how do you deal with errors that happen after you've started
-// streaming the response? Also a problem for single files though, but less bad.
-//
-// Instead try to make the single-flow-file use case more usable.
-//
-
-// Proposed solution:
-// The request returns 200 unless it immediately fails parsing, due to things like wrong encoding.
-// Otherwise, we return one flow file per incoming flow file. Status and potential return message
-// per flow file should be conveyed in the attributes of the returned file.
-//
-// Requirements:
-//
-// 1. Make it easy to just alter the attributes of a flow file and return it, either with or
-//    without the body.
-// 2. Somehow allow you to _modify_ the returned flowfile.
-//
-//
-// For the latter part there are several use cases:
-//
-// 1. Alter the body in a streaming fashion.
-// 2. Buffer the entire contents in memory.
-// 3. Buffer the entire contents to a "spooled file"
-//    This could be a fallback for the memory version, with a capped memory amount.
-// 4. Produce arbitrary output.
-
 #[tracing::instrument(ret, skip_all)]
 async fn process_single(
-    ff: StreamedFlowFileFuture,
-) -> Result<impl IntoResponse, FlowFileParsingError> {
+    ff: nifioxide::axum::StreamedFlowFileFuture,
+) -> Result<impl IntoResponse, nifioxide::FlowFileParsingError> {
     let mut ff = ff.await?;
 
     tracing::info!("Flow file with size: {}", ff.size());
@@ -87,8 +53,8 @@ async fn process_single(
 
 #[tracing::instrument(ret, skip_all)]
 async fn process_multiple(
-    flow_files: FlowFileStream,
-) -> Result<impl IntoResponse, FlowFileParsingError> {
+    flow_files: nifioxide::FlowFileStream,
+) -> Result<impl IntoResponse, nifioxide::FlowFileParsingError> {
     flow_files
         .then(|ff| async move {
             let mut ff = ff?;
@@ -99,7 +65,7 @@ async fn process_multiple(
             let mut buf = [0u8; 128];
             let n = ff.contents().read(&mut buf).await?;
             tracing::debug!("read {n} bytes from file body");
-            Ok::<_, FlowFileParsingError>(())
+            Ok::<_, nifioxide::FlowFileParsingError>(())
         })
         .try_collect::<()>()
         .await?;
@@ -110,7 +76,5 @@ async fn process_multiple(
         HeaderValue::from_static("axum+nifioxide"),
     );
 
-    // Note that this returns OK right away, before anything has been processed at all.
-    // This only really makes sense if errors are encoded in the body somehow.
     Ok((axum::http::StatusCode::OK, response_headers))
 }
--- src/flowfiles/flowfile.rs
+++ src/flowfiles/flowfile.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, marker::PhantomData};
 
-use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite};
 
 use super::FlowFileHeader;
 
@@ -8,10 +8,10 @@ use super::FlowFileHeader;
 ///
 /// This is generic over the type that holds the content. The header is kept in memory as a
 /// [`FlowFileHeader`].
-pub struct FlowFile<R> {
+pub struct FlowFile<C> {
     header: FlowFileHeader,
     /// The content of the flow file.
-    content: R,
+    content: C,
 }
 
 impl FlowFile<()> {
@@ -28,11 +28,19 @@ impl FlowFile<()> {
     }
 }
 
-impl<R> FlowFile<R> {
+impl<C> FlowFile<C> {
+    #[must_use]
+    pub fn builder() -> FlowFileBuilder<Unset<u64>, Unset<HashMap<String, String>>, Unset<C>> {
+        FlowFileBuilder {
+            size: Unset(PhantomData),
+            attributes: Unset(PhantomData),
+            content: Unset(PhantomData),
+        }
+    }
     pub fn new(
         size: impl Into<u64>,
         attributes: impl Into<HashMap<String, String>>,
-        content: R,
+        content: C,
     ) -> Self {
         Self {
             header: (size, attributes).into(),
@@ -45,18 +53,18 @@ impl<R> FlowFile<R> {
     pub fn header_mut(&mut self) -> &mut FlowFileHeader {
         &mut self.header
     }
-    pub fn content(&self) -> &R {
+    pub fn content(&self) -> &C {
         &self.content
     }
-    pub fn content_mut(&mut self) -> &mut R {
+    pub fn content_mut(&mut self) -> &mut C {
         &mut self.content
     }
-    pub fn into_parts(self) -> (FlowFileHeader, R) {
+    pub fn into_parts(self) -> (FlowFileHeader, C) {
         (self.header, self.content)
     }
 }
 
-impl<R> std::ops::Deref for FlowFile<R> {
+impl<C> std::ops::Deref for FlowFile<C> {
     type Target = FlowFileHeader;
 
     fn deref(&self) -> &Self::Target {
@@ -80,5 +88,138 @@ impl<R: AsyncRead + Unpin> FlowFile<R> {
         self.header.serialize_into(&mut w).await?;
         tokio::io::copy(&mut self.content, &mut w).await?;
         Ok(())
+    }
+}
+
+impl<T: Into<HashMap<String, String>>> From<T> for FlowFile<()> {
+    fn from(value: T) -> Self {
+        FlowFile::empty_with_attributes(value.into())
+    }
+}
+
+#[doc(hidden)]
+pub struct Unset<T>(PhantomData<T>);
+#[doc(hidden)]
+pub struct Set<T>(T);
+
+#[doc(hidden)]
+pub struct FlowFileBuilder<Size, Attributes, Content> {
+    size: Size,
+    attributes: Attributes,
+    content: Content,
+}
+impl<S, C> FlowFileBuilder<S, Unset<HashMap<String, String>>, C> {
+    pub fn attributes(
+        self,
+        attributes: impl Into<HashMap<String, String>>,
+    ) -> FlowFileBuilder<S, Set<HashMap<String, String>>, C> {
+        FlowFileBuilder {
+            size: self.size,
+            attributes: Set(attributes.into()),
+            content: self.content,
+        }
+    }
+}
+impl<A, C> FlowFileBuilder<Unset<u64>, A, C> {
+    pub fn size(self, size: impl Into<u64>) -> FlowFileBuilder<Set<u64>, A, C> {
+        FlowFileBuilder {
+            size: Set(size.into()),
+            attributes: self.attributes,
+            content: self.content,
+        }
+    }
+}
+impl<S, A, C> FlowFileBuilder<S, A, Unset<C>> {
+    pub fn content(self, content: C) -> FlowFileBuilder<S, A, Set<C>> {
+        FlowFileBuilder {
+            size: self.size,
+            attributes: self.attributes,
+            content: Set(content),
+        }
+    }
+}
+
+impl<C> FlowFileBuilder<Set<u64>, Set<HashMap<String, String>>, Set<C>> {
+    pub fn build(self) -> FlowFile<C> {
+        FlowFile {
+            header: FlowFileHeader::new(self.size.0, self.attributes.0),
+            content: self.content.0,
+        }
+    }
+}
+
+impl<A, C> FlowFileBuilder<Unset<u64>, A, Unset<C>> {
+    pub fn content_from_slice(
+        self,
+        content: &[u8],
+    ) -> FlowFileBuilder<Set<u64>, A, std::io::Cursor<Vec<u8>>> {
+        FlowFileBuilder {
+            size: Set(content.len() as u64),
+            attributes: self.attributes,
+            content: std::io::Cursor::new(Vec::from(content)),
+        }
+    }
+}
+
+impl<A, C> FlowFileBuilder<Unset<u64>, A, Unset<C>> {
+    pub fn content_from_vec(
+        self,
+        content: Vec<u8>,
+    ) -> FlowFileBuilder<Set<u64>, A, std::io::Cursor<Vec<u8>>> {
+        FlowFileBuilder {
+            size: Set(content.len() as u64),
+            attributes: self.attributes,
+            content: std::io::Cursor::new(content),
+        }
+    }
+}
+
+impl<A, C> FlowFileBuilder<Unset<u64>, A, Unset<C>> {
+    pub async fn content_from_file(
+        self,
+        content: impl Into<tokio::fs::File>,
+    ) -> tokio::io::Result<FlowFileBuilder<Set<u64>, A, Set<tokio::fs::File>>> {
+        let file = content.into();
+        let size = file.metadata().await?.len();
+        Ok(FlowFileBuilder {
+            size: Set(size as u64),
+            attributes: self.attributes,
+            content: Set(file),
+        })
+    }
+}
+
+impl<A, C> FlowFileBuilder<Unset<u64>, A, Unset<C>> {
+    pub async fn content_from_reader_buffered_in_memory<R: AsyncRead + Unpin>(
+        self,
+        mut reader: R,
+    ) -> tokio::io::Result<FlowFileBuilder<Set<u64>, A, Set<std::io::Cursor<Vec<u8>>>>> {
+        let mut buf = Vec::new();
+        reader.read_to_end(&mut buf).await?;
+        Ok(FlowFileBuilder {
+            size: Set(buf.len() as u64),
+            attributes: self.attributes,
+            content: Set(std::io::Cursor::new(buf)),
+        })
+    }
+}
+
+#[cfg(feature = "tempfile")]
+impl<A, C> FlowFileBuilder<Unset<u64>, A, Unset<C>> {
+    pub async fn content_from_reader_buffered_in_tempfile<R: AsyncRead + Unpin>(
+        self,
+        mut reader: R,
+    ) -> tokio::io::Result<FlowFileBuilder<Set<u64>, A, Set<tokio::fs::File>>> {
+        use std::io::SeekFrom;
+        use tokio::io::AsyncSeekExt;
+
+        let mut file: tokio::fs::File = tempfile::tempfile()?.into();
+        let size = tokio::io::copy(&mut reader, &mut file).await?;
+        file.seek(SeekFrom::Start(0)).await?;
+        Ok(FlowFileBuilder {
+            size: Set(size),
+            attributes: self.attributes,
+            content: Set(file),
+        })
     }
 }
