db2782686481852cb92c91677a49a23f76b6a9cc
Bendik Samseth <bendik@samseth.me>
2026-07-30 00:05:00 +0200

    Remove everything pending merge with nififf3

--- src/flowfiles/mod.rs
+++
@@ -1,7 +0,0 @@
-mod error;
-mod flowfile;
-mod header;
-
-pub use error::FlowFileParsingError;
-pub use flowfile::FlowFile;
-pub use header::FlowFileHeader;
--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![expect(clippy::doc_markdown, reason = "NiFi is spelled in camelCase")]
-#[doc = include_str!("../README.md")]
-pub mod axum;
-mod flowfiles;
-
-pub use flowfiles::*;
